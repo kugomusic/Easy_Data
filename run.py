d87454dae5483d618e678c5d4f38c2896979d518
田川 <tc@tianchuandeMacBook-Air.local>
2019-03-07 16:26:05 +0800

    提交文件

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
 
 from app import app
-app.run(debug=True, host='127.0.0.1', port=8080)
+# app.run(debug=True, host='127.0.0.1', port=8080)
+app.run(debug=True, host='10.28.185.81', port=8080)
